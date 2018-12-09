edf40fbe392cbd78f69eb212a7aa60255d8dc1a5
Вова Петров <wolfram@macbook-pro-vova.local>
2018-12-09 19:49:00 +0500

    delete data

--- DataBases/Modules/CreateTableViewController.swift
+++ DataBases/Modules/CreateTableViewController.swift
@@ -40,8 +40,8 @@ class CreateTableViewController: UITableViewController {
     }
     
     @IBAction func createColumnAction(_ sender: Any) {
-        if dataTypeSegment.selectedSegmentIndex == 4 {
-            
+        if dataTypeSegment.selectedSegmentIndex == 3 {
+           // self.view
         }
         
     }
@@ -58,8 +58,6 @@ class CreateTableViewController: UITableViewController {
         switch columnType {
         case .bool:
             return "Да/Нет"
-        case .date:
-            return "Дата"
         case .id:
             return "Ссылка"
         case .integer:
@@ -72,12 +70,10 @@ class CreateTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return columnArray.count
     }
 
@@ -85,45 +81,9 @@ class CreateTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell.init(style: .value1, reuseIdentifier: nil)
         cell.textLabel?.text = columnArray[indexPath.row].name
-        cell.detailTextLabel?.text = getNameAt(columnType: columnArray[indexPath.row].type)
+       // cell.detailTextLabel?.text = getNameAt(columnType: columnArray[indexPath.row].type)
         return cell
     }
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     // MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
