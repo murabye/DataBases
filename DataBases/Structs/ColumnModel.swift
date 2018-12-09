3f52d6a09c05d2412913c82f3be121b0cf47f95c
varya <vlada_kuz2011@mail.ru>
2018-12-09 20:35:15 +0500

    redo columnModel name

---
+++ DataBases/Structs/ColumnModel.swift
@@ -0,0 +1,30 @@
+//
+//  dbColumn.swift
+//  DataBases
+//
+//  Created by варя on 06/11/2018.
+//  Copyright © 2018 варя. All rights reserved.
+//
+
+import UIKit
+
+// TODO: узнать, как они называются в sqlite
+enum columnType:String {
+    case text
+    case integer
+    case bool
+    case id
+}
+
+struct ColumnModel {
+    
+    let id_table: Int32 = 0
+    let name: String = ""
+    // TODO: убрать из таблицы let default_value:String = ""
+    let type: columnType = .id
+    var mask: maskModel?
+    let unique: Bool = false
+    let not_null: Bool = false
+    // TODO: убрать из таблицы let auto_increment:Bool = false
+    let primary_key: Bool = false
+}
