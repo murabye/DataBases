982f70809247c5f63de2dd028972c7ad1b8741e0
Вова Петров <wolfram@macbook-pro-vova.local>
2018-12-15 23:11:35 +0500

    123

--- DataBases/Modules/ChooseDbScreen/ChooseDbScreenView.swift
+++ DataBases/Modules/ChooseDbScreen/ChooseDbScreenView.swift
@@ -10,18 +10,60 @@ import UIKit
 
 //MARK: ChooseDbScreenView Class
 final class ChooseDbScreenView: BaseTableView {
+    @IBOutlet weak var secondAddButton: UIButton!
     @IBOutlet weak var addButton: UIBarButtonItem!
     override func viewDidLoad() {
         let module = AppModules.ChooseDbScreen.build()
         self._module = module
         super.viewDidLoad()
         module.change(view: self)
+        updateButtonsState()
     }
     
     @IBAction func adminChange(_ sender: Any) {
-      //  if let segmentControl = sender as //UISegmentedControl{
-          //  segmentControl.sele
-       // }
+        let segmentControl = sender as! UISegmentedControl
+        if segmentControl.selectedSegmentIndex == 1 {
+            let alert = UIAlertController(title: "Вход администратора", message: "Введите пароль администратора", preferredStyle: .alert)
+            let cancelAction = UIAlertAction(title: "Отмена", style: .cancel) { (_) -> Void in
+            }
+            let saveAction = UIAlertAction(title: "Войти", style: .default) { (_) -> Void in
+                let nameField = alert.textFields?[0]
+                
+                if let password = nameField?.text {
+                    if password == "111"{
+                        SqlManager.shared.isAdmin = true
+                        self.updateButtonsState()
+                        return
+                    } else {
+                        SqlManager.shared.isAdmin = false
+                        segmentControl.selectedSegmentIndex = 0
+                        self.updateButtonsState()
+                    }
+                }
+                
+            }
+            
+            alert.addTextField { (_) -> Void in }
+            alert.textFields![0].placeholder = "Пароль"
+            
+            alert.addAction(cancelAction)
+            alert.addAction(saveAction)
+            
+            self.present(alert, animated: true, completion: nil)
+        } else {
+            SqlManager.shared.isAdmin = false
+            self.updateButtonsState()
+        }
+    }
+    
+    func updateButtonsState() {
+        if SqlManager.shared.isAdmin {
+            self.addButton.isEnabled = true
+            self.secondAddButton.isEnabled = true
+        } else {
+            self.addButton.isEnabled = false
+            self.secondAddButton.isEnabled = false
+        }
     }
     @IBAction func addDbButtonAction(_ sender: Any) {
         let alert = UIAlertController(title: "Новое хранилище", message: "Введите название нового хранилища", preferredStyle: .alert)
